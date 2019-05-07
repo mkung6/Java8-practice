81761f52fcff478722b361a60aff7e0702f46da7
mkung <tarrant6@gmail.com>
2019-05-07 15:39:08 -0700

    Update SumStrings.java
    attempting to implement decimals

--- SumStrings.java
+++ SumStrings.java
@@ -1,24 +1,12 @@
 import java.util.*;
+import java.lang.*;
 
 class SumStrings {
     public static void main(String[] args) {
-        String str1 = "0.9";
-        String str2 = "999";
+        String str1 = "5.123";
+        String str2 = "999.9234";
         System.out.println(sumStrings(str1, str2));
     }
-
-    // public static int calculate(String str1, String str2) {
-    //     char[] c1 = str1.toCharArray();
-    //     char[] c2 = str2.toCharArray();
-    //     int c1DecimalPlace = decimalPlace(c1);
-    //     int c2DecimalPlace = decimalPlace(c2);
-    //     int decimalSum = 0;
-    //     int sum = 0;
-    //     if (c1DecimalPlace != -1 && c2DecimalPlace != -1) {
-    //         decimalSum = sumStrings(str1.substring(c1DecimalPlace + 1).toCharArray(), str2.sustring(c2DecimalPlace + 1).toCharArray());
-    //         sum = sumStrings(str1.substring(0, c1DecimalPlace - 1).toCharArray(), str2.substring(0, c2DecimalPlace - 1).toCharArray());
-    //     }
-    // }
 
     // return the index of decimal place
     public static int decimalPlace(char[] c) {
@@ -37,7 +25,52 @@ class SumStrings {
         return false;
     }
 
-    public static int sumStrings(char[] c1, char[] c2) {
+    public static double calculateDecimal(char[] c1, char[] c2) {
+        int i = c1.length - 1;
+        int j = c2.length - 1;
+        double tens = 0.1;
+        double sum = 0;
+        int carryOver = 0;
+        if(i > j) {
+            for(int k = 0; k < i; k++) {
+                tens = tens / 10;
+            }
+        }
+        else {
+            for(int k = 0; k < j; k++) {
+                tens = tens / 10;
+            }
+        }
+        while (i >= 0 || j >= 0) {
+            int sumDigit = 0;
+            if (i > j && i >= 0) {
+                sumDigit = (c1[i] - '0') * tens;
+                sum += sumDigit;
+                tens *= 10;
+                i--;
+            }
+            else if (i < j && j >= 0) {
+                sumDigit = (c2[j] - '0') * tens;
+                sum += sumDigit;
+                tens *= 10;
+                j--;
+            }
+            else {
+                int num1 = 0;
+                int num2 = 0;
+                if (i >= 0) {
+                    num1 = c1[i] - '0';
+                }
+                if (j >= 0) {
+                    num2 = c2[j] - '0';
+                }
+                sumDigit = (num1 + num2 + carryOver);
+
+            }
+        }
+    }
+
+    public static int calculate(char[] c1, char[] c2) {
         int i = c1.length - 1;
         int j = c2.length - 1;
         int sum = 0;
@@ -54,27 +87,21 @@ class SumStrings {
             if (j >= 0) {
                 num2 = c2[j] - '0';
             }
-            System.out.println("num1: " + num1);
-            System.out.println("num2: " + num2);
             sumDigit = (num1 + num2 + carryOver);
-            System.out.println("sumDigit: " + sumDigit);
             // edge case when str1 and str2 are just one digit
             // then it'd calculate the carry over but not add it to our sum
             // as carryOver would never get evaluated a second time
             // carry over value also has to scale with tens to calculate
             // correct value
             if (sumDigit > 9 && c1.length > 1 && c2.length > 1) {
-                System.out.println("inside if");
                 carryOver = sumDigit / (tens * 10);
                 sumDigit = sumDigit % (tens * 10);
             }
             else {
                 carryOver = 0;
             }
-            System.out.println("carryOver: " + carryOver);
             sumDigit *= tens;
             sum += sumDigit;
-            System.out.println("sum: " + sum);
             tens *= 10;
             j--;
             i--;
@@ -82,29 +109,46 @@ class SumStrings {
         return sum;
     }
 
-    // public static int getNum(Character c) {
-    //     switch(c) {
-    //         case '1':
-    //             return 1;
-    //         case '2':
-    //             return 2;
-    //         case '3':
-    //             return 3;
-    //         case '4':
-    //             return 4;
-    //         case '5':
-    //             return 5;
-    //         case '6':
-    //             return 6;
-    //         case '7':
-    //             return 7;
-    //         case '8':
-    //             return 8;
-    //         case '9':
-    //             return 9;
-    //         case '0':
-    //             return 0;
-    //     }
-    //     return -1;
-    // }
+    public static int sumStrings(String str1, String str2) {
+        char[] c1 = str1.toCharArray();
+        char[] c2 = str2.toCharArray();
+        int i = c1.length - 1;
+        int j = c2.length - 1;
+
+        int c1DecimalPlace = decimalPlace(c1);
+        int c2DecimalPlace = decimalPlace(c2);
+        // if both numbers have decimals
+        if(c1DecimalPlace != -1 && c2DecimalPlace != -1) {
+            c1DecimalPlace++;
+            c2DecimalPlace++;
+            char[] c1DecimalCopy = new char[c1.length - c1DecimalPlace];
+            char[] c2DecimalCopy = new char[c2.length - c2DecimalPlace];
+            for(int k = 0; k < c1DecimalCopy.length; k++) {
+                c1DecimalCopy[k] = c1[c1DecimalPlace + k];
+            }
+            for(int k = 0; k < c2DecimalCopy.length; k++) {
+                c2DecimalCopy[k] = c2[c2DecimalPlace + k];
+            }
+            int calculateDecimals = calculate(c1DecimalCopy, c2DecimalCopy);
+            if(calculateDecimals > c1DecimalCopy.length || calculateDecimals > c2DecimalCopy.length) {
+                c1DecimalPlace--;
+                char[] c1WithoutDecimal = new char[c1DecimalPlace];
+                for(int k = 0; k < c1WithoutDecimal.length; k++) {
+                    c1WithoutDecimal[k] = c1[k];
+                }
+                char[] calculateDecimalsAsString = Integer.toString(calculateDecimals).toCharArray();
+                char[] takeCarryOver = new char[1];
+                takeCarryOver[0] = calculateDecimalsAsString[0];
+                System.out.println(calculateDecimalsAsString);
+                System.out.println(c1WithoutDecimal);
+                System.out.println(takeCarryOver);
+                int addDecimalCarryOver = calculate(c1WithoutDecimal, takeCarryOver);
+                System.out.println(addDecimalCarryOver);
+            }
+        }
+
+
+
+        return 0;
+    }
 }
